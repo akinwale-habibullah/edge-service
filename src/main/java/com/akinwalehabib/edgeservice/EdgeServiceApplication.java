c40edcb5c3d5a274ae8759fa16899c84a7d320c3
Akinwale Habib <akinwalehabib@hotmail.com>
2023-09-16 15:52:29 +0100

    add cloud config, and define commit stage deployment pipeline

--- src/main/java/com/akinwalehabib/edgeservice/EdgeServiceApplication.java
+++ src/main/java/com/akinwalehabib/edgeservice/EdgeServiceApplication.java
@@ -2,8 +2,10 @@ package com.akinwalehabib.edgeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class EdgeServiceApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/akinwalehabib/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/akinwalehabib/edgeservice/EdgeServiceApplicationTests.java
@@ -1,26 +1,58 @@
 package com.akinwalehabib.edgeservice;
 
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.testcontainers.containers.GenericContainer;
+// import org.testcontainers.junit.jupiter.Container;
+// import org.testcontainers.junit.jupiter.Testcontainers;
+// import org.testcontainers.utility.DockerImageName;
+// import org.springframework.test.context.DynamicPropertySource;
+// import org.springframework.test.context.DynamicPropertyRegistry;
+
+// @SpringBootTest(
+// 	webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+// )
+// @Testcontainers
+// class EdgeServiceApplicationTests {
+
+// 	private static final int REDIS_PORT = 6379;
+
+// 	@Container
+// 	static GenericContainer<?> redis = 
+// 	new GenericContainer<>(DockerImageName.parse("redis:7.0"))
+// 		.withExposedPorts(REDIS_PORT);
+
+// 	@DynamicPropertySource
+// 	static void redisProperties(DynamicPropertyRegistry registry) {
+// 		registry.add("spring.redis.host", () -> redis.getHost());
+// 		registry.add("spring.redis.port", () -> redis.getMappedPort(REDIS_PORT));
+// 	}
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
+
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
-import org.springframework.test.context.DynamicPropertyRegistry;
 
-@SpringBootTest(
-	webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 class EdgeServiceApplicationTests {
 
 	private static final int REDIS_PORT = 6379;
 
 	@Container
-	static GenericContainer<?> redis = 
-	new GenericContainer<>(DockerImageName.parse("redis:7.0"))
-		.withExposedPorts(REDIS_PORT);
+	static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7.0"))
+			.withExposedPorts(REDIS_PORT);
 
 	@DynamicPropertySource
 	static void redisProperties(DynamicPropertyRegistry registry) {
@@ -29,7 +61,7 @@ class EdgeServiceApplicationTests {
 	}
 
 	@Test
-	void contextLoads() {
+	void verifyThatSpringContextLoads() {
 	}
 
 }
